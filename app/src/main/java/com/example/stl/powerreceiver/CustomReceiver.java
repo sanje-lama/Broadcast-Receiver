30104de37a59ca5d38a81a2c48482a5c6a6361c0
sanje-lama <sanjelama@pursuit.org>
2018-12-16 17:42:05 -0500

    initial commit

---
+++ app/src/main/java/com/example/stl/powerreceiver/CustomReceiver.java
@@ -0,0 +1,37 @@
+package com.example.stl.powerreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+import static com.example.stl.powerreceiver.MainActivity.ACTION_CUSTOM_BROADCAST;
+
+public class CustomReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String intentAction = intent.getAction();
+
+        if (intentAction!= null) {
+            String toastMessage = "unknown intent action";
+
+            switch (intentAction) {
+                case Intent.ACTION_POWER_CONNECTED:
+                    toastMessage = "Power connected";
+                    break;
+
+                case Intent.ACTION_POWER_DISCONNECTED:
+                    toastMessage = "Power disconnected";
+                    break;
+
+                case ACTION_CUSTOM_BROADCAST:
+                toastMessage =  "custom_broadcast_toast";
+                break;
+            }
+            Toast.makeText(context, toastMessage, Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+}
